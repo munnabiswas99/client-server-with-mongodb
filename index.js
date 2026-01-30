9a30339330480acec5600ec1e7cb9d10a5bce7b0
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-01-30 17:54:34 +0600

    User created to the DB

--- index.js
+++ index.js
@@ -18,7 +18,23 @@ const client = new MongoClient(uri, {
   }
 });
 
+async function run() {
+    try{
+        await client.connect();
 
+        app.post('/users', (req, res) => {
+            console.log('data in the server', req.body);
+        })
+
+        await client.db('admin').command({ping: 1})
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
 
 app.use(cors());
 app.use(express.json());
